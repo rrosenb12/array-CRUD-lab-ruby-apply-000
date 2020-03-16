e67826f2f29ac1289990b6ca89224bca4f0030db
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-03-16 02:41:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-
+  array = ["wow", "pony", "kitty", "piggy", "puppy", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
